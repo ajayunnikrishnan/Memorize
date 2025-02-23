6a41a476bf2bc6ce712659e436a6b26d224715ea
Ajay Unnikrishnan <ajayu.canada@gmail.com>
2025-02-22 18:18:12 -0800

    Initial Commit

---
+++ Memorize/MemorizeApp.swift
@@ -0,0 +1,17 @@
+//
+//  MemorizeApp.swift
+//  Memorize
+//
+//  Created by Ajay Unnikrishnan on 2025-02-22.
+//
+
+import SwiftUI
+
+@main
+struct MemorizeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
